eccede9dc047c45d380dd837588aa39ac5b5b4f7
David Muirhead <davidmuirhead@Davids-MBP.gateway>
2025-02-09 23:55:31 +1000

    tester

--- site/web/app/themes/sh80/resources/views/index.blade.php
+++ site/web/app/themes/sh80/resources/views/index.blade.php
@@ -3,13 +3,13 @@
 @include('sections.header')
 </section>
 <!-- @section('content') -->
-<section id="bigRed" class="feature release text-sh80-green flex w-full my-60">
+<section id="bigRed" class="feature release text-sh80-cula flex w-full my-60">
 @include('sections.promo-releases')
 </section>
-<section id="bigRed" class="feature release text-sh80-green flex w-full my-60">
+<section id="bigRed" class="feature release text-sh80-cula flex w-full my-60">
 @include('sections.featured-releases')
 </section>
-<section id="d8s" class="mt-36 rap w-full p-8 text-sh80-blue z-10 min-h-screen flex items-center justify-center my-60">
+<section id="d8s" class="mt-36 rap w-full p-8 text-sh80-cula z-10 min-h-screen flex items-center justify-center my-60">
 @include('sections.d8s')
 </section>
 <section id="c0nt3nt" class="p-2 my-80">
